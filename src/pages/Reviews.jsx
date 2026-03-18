0560f68c588d7571ce0508f40c036a12f13998ad
Youhavevalue <drmarcusb@drmarcusb.com>
2026-03-18 08:04:36 -0500

    Fix 404 Unsplash image for Ralph

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -7,7 +7,7 @@ const Reviews = () => {
     {
       name: 'Ralph E.',
       score: '+114 Pts',
-      image: 'https://images.unsplash.com/photo-1504257432389-523431e51d05?auto=format&fit=crop&q=80&w=150&h=150',
+      image: 'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?auto=format&fit=crop&q=80&w=150&h=150',
       content: 'Home Ready Scores removed 7 negative items in as little as 45 days. I was finally able to qualify for my first home at a 4.2% interest rate. They changed my family\'s life.',
       type: 'Homeowner'
     },
